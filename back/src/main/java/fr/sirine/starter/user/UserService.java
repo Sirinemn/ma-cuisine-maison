335f622a648adcd8660363c70f3379d757a37cb3
Sirinemn <sirinemn@outlook.fr>
2024-10-19 00:44:09 +0200

    update userservice

--- back/src/main/java/fr/sirine/starter/user/UserService.java
+++ back/src/main/java/fr/sirine/starter/user/UserService.java
@@ -1,23 +1,55 @@
 package fr.sirine.starter.user;
 
 
+import fr.sirine.starter.dto.UserDto;
+import fr.sirine.starter.mapper.UserMapper;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
-import java.util.HashSet;
-import java.util.Set;
+import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 
 @Service
 @Transactional
 public class UserService {
 
     private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, UserMapper userMapper) {
         this.userRepository = userRepository;
+        this.userMapper = userMapper;
+    }
+    public List<UserDto> getAllUsers() {
+        return userRepository.findAll().stream()
+                .map(user -> {
+                    try {
+                        return userMapper.toDto(user);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        return null;
+                    }
+                })
+                .filter(Objects::nonNull) // Ensure nulls are filtered out
+                .collect(Collectors.toList());
     }
 
+    public void updateUser(Integer id, UserDto userDto){
+        User initialUser = userRepository.findById(id).orElse(null);
+        if (initialUser!= null) {
+            User user = userMapper.toEntity(userDto);
+            initialUser.setPseudo(user.getPseudo());
+            initialUser.setEmail(user.getEmail());
+            initialUser.setFirstname(user.getFirstname());
+            initialUser.setPassword(user.getPassword());
+            initialUser.setLastname(user.getLastname());
+            initialUser.setDateOfBirth(user.getDateOfBirth());
+            userRepository.save(initialUser);
+        }
+    }
     public void deleteUser(Integer id) {
 
         this.userRepository.deleteById(id);
