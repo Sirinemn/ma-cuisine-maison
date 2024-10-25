1e1664134a50263f2e34917d2edb3ffe6a5d37de
Sirinemn <sirinemn@outlook.fr>
2024-10-25 21:57:01 +0200

    update notfound page and home page

--- front/src/app/pages/not-found/not-found.component.spec.ts
+++ front/src/app/pages/not-found/not-found.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { NotFoundComponent } from './not-found.component';
+import { CommonModule } from '@angular/common';
+import { provideRouter } from '@angular/router';
 
 describe('NotFoundComponent', () => {
   let component: NotFoundComponent;
@@ -8,7 +10,12 @@ describe('NotFoundComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-    imports: [NotFoundComponent]
+    imports: [NotFoundComponent,
+      CommonModule    
+    ],
+    providers: [
+      provideRouter([]), // Configurer les routes pour le test
+    ]
 });
     fixture = TestBed.createComponent(NotFoundComponent);
     component = fixture.componentInstance;
