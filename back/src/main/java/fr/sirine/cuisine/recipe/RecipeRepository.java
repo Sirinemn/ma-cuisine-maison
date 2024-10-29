32c7f6c3233a75871441129e3f20a3881ba8b8d2
Sirinemn <sirinemn@outlook.fr>
2024-10-29 17:06:19 +0100

    implement recipeService

--- back/src/main/java/fr/sirine/cuisine/recipe/RecipeRepository.java
+++ back/src/main/java/fr/sirine/cuisine/recipe/RecipeRepository.java
@@ -3,6 +3,12 @@ package fr.sirine.cuisine.recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Set;
+
 @Repository
 public interface RecipeRepository extends JpaRepository<Recipe, Integer> {
+    List<Recipe> findByCategoryName(String categoryName);
+
+    Set<Recipe> findByUserId(Integer userId);
 }
--- back/src/main/java/fr/sirine/cuisine/recipe/RecipeService.java
+++ back/src/main/java/fr/sirine/cuisine/recipe/RecipeService.java
@@ -3,17 +3,45 @@ package fr.sirine.cuisine.recipe;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+
 @Service
 @Transactional
 public class RecipeService {
 
     private final RecipeRepository recipeRepository;
+    private final RecipeMapper recipeMapper;
 
-    public RecipeService(RecipeRepository recipeRepository) {
+    public RecipeService(RecipeRepository recipeRepository, RecipeMapper recipeMapper) {
         this.recipeRepository = recipeRepository;
+        this.recipeMapper = recipeMapper;
     }
 
-    public Recipe findById(Integer id){
-        return this.recipeRepository.findById(id).orElse(null);
+    public List<RecipeDto> findAll(){
+         List<Recipe> recipes = recipeRepository.findAll();
+        return recipes.stream().map(recipeMapper::toDto).collect(Collectors.toList());
+    }
+
+    public List<RecipeDto> findRecipeByCategory(String categoryName) {
+        List<Recipe> recipes = recipeRepository.findByCategoryName(categoryName);
+        return recipes.stream().map(recipeMapper::toDto).collect(Collectors.toList()); }
+
+    public RecipeDto findById(Integer id) {
+        Recipe recipe = recipeRepository.findById(id).orElse(null);
+        return recipeMapper.toDto(recipe);
+    }
+
+    public Set<RecipeDto> getRecipesByUserId(Integer userId){
+        Set<Recipe> recipes = recipeRepository.findByUserId(userId);
+        return recipes.stream().map(recipeMapper::toDto).collect(Collectors.toSet());
+    }
+
+    public void deleteRecipe(Integer id) {
+        recipeRepository.deleteById(id);
     }
 }
