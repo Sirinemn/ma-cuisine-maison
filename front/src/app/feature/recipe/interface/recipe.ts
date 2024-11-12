eca50a5353c3246c8376457add06c201703d20b6
Sirinemn <sirinemn@outlook.fr>
2024-11-12 23:15:09 +0100

    add recipe interface

---
+++ front/src/app/feature/recipe/interface/recipe.ts
@@ -0,0 +1,11 @@
+export interface Recipe{
+    id: number,
+    title: string,
+    description: string,
+    cookingTime: number,
+    serving: number,
+    userId: number,
+    userPseudo: string,
+    
+
+}
