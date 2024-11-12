e1099195650dc2434c53cffef94d5e6716ae2aa1
Sirinemn <sirinemn@outlook.fr>
2024-11-12 23:37:18 +0100

    add form component

---
+++ front/src/app/feature/recipe/components/form/form.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.scss'
+})
+export class FormComponent {
+}
