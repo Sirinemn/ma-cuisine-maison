f6d5be8b7729d32ce81566d16992d2bca8ff7743
Sirinemn <sirinemn@outlook.fr>
2024-11-19 22:02:28 +0100

    delete form.component.html

--- front/src/app/feature/recipe/components/form/form.component.ts
+++ front/src/app/feature/recipe/components/form/form.component.ts
@@ -1,11 +1,16 @@
+import { NgForOf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
+  imports: [
+  ],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss'
 })
-export class FormComponent {
+export class FormComponent  {
+
 }
