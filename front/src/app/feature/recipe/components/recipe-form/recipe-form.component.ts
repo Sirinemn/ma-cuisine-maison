b9c65f00db505db9d94839729a2b34c498605d34
Sirinemn <sirinemn@outlook.fr>
2024-11-26 16:41:34 +0100

    update recipe-form.component.html

--- front/src/app/feature/recipe/components/recipe-form/recipe-form.component.ts
+++ front/src/app/feature/recipe/components/recipe-form/recipe-form.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-recipe-form',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule,
+  ],
   templateUrl: './recipe-form.component.html',
   styleUrl: './recipe-form.component.scss'
 })
 export class RecipeFormComponent {
+
+  public back() {
+    window.history.back();
+  }
+  public Submit(){
+    
+  }
 
 }
