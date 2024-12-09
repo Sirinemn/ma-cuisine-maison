879caf368f96ea1dbe09a229baee5a563cd9ce4c
Sirinemn <sirinemn@outlook.fr>
2024-12-09 15:28:41 +0100

    fix recipeMapper

--- back/src/main/java/fr/sirine/cuisine/recipe/RecipeMapper.java
+++ back/src/main/java/fr/sirine/cuisine/recipe/RecipeMapper.java
@@ -33,7 +33,6 @@ public abstract class RecipeMapper implements EntityMapper<RecipeDto, Recipe> {
             @Mapping(target = "user", expression = "java(recipeDto.getUserId() != null ? userService.findById(recipeDto.getUserId()) : null)"),
             @Mapping(target = "image", expression = "java(recipeDto.getImageId() != null ? imageService.findById(recipeDto.getImageId()) : null)"),
             @Mapping(target = "category", expression = "java(recipeDto.getCategoryName() != null ? categoryService.findByName(recipeDto.getCategoryName()) : null)"),
-            @Mapping(target = "ingredients", ignore = true) // gérer les ingrédients séparément si nécessaire
     })
     public abstract Recipe toEntity(RecipeDto recipeDto);
 
@@ -43,20 +42,8 @@ public abstract class RecipeMapper implements EntityMapper<RecipeDto, Recipe> {
             @Mapping(source = "recipe.image.id", target = "imageId"),
             @Mapping(target = "imageThumbUrl", expression = "java(recipe.getImage()!= null ? recipe.getImage().getThumbnailLocation() : null)"),
             @Mapping(target = "imageUrl", expression = "java(recipe.getImage()!= null ? recipe.getImage().getImageLocation() : null)"),
-            @Mapping(expression = "java(recipe.getCategory() != null ? recipe.getCategory().getName().name() : null)", target = "categoryName"),
-            @Mapping(target = "ingredients", ignore = true) // Gérez les ingrédients séparément si besoin
+            @Mapping(target = "categoryName", expression = "java(recipe.getCategory() != null ? recipe.getCategory().getName().name() : null)"),
     })
     public abstract RecipeDto toDto(Recipe recipe);
-
-    //  Méthode utilitaire pour convertir la liste des ingrédients si nécessaire
-    protected List<IngredientDto> mapIngredients(List<RecipeIngredient> recipeIngredients) {
-        return recipeIngredients.stream()
-                .map(ri -> new IngredientDto(
-                        ri.getIngredient().getId(),
-                        ri.getIngredient().getName(),
-                        ri.getQuantity(),
-                        ri.getUnit()
-                ))
-                .collect(Collectors.toList());
-    }
+    
 }
