12204e53c538c1f37be993b7b8cb1744ff2b7fea
Sirinemn <sirinemn@outlook.fr>
2025-01-13 10:15:23 +0100

    implement user.service.spec.ts

--- front/src/app/service/user.service.spec.ts
+++ front/src/app/service/user.service.spec.ts
@@ -1,11 +1,13 @@
 import { TestBed } from '@angular/core/testing';
-
 import { UserService } from './user.service';
 import { provideHttpClient } from '@angular/common/http';
-import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { User } from '../interface/user';
+import { MessageResponse } from '../interface/api/messageResponse.interface';
 
 describe('UserService', () => {
   let service: UserService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -15,9 +17,46 @@ describe('UserService', () => {
       ]
     });
     service = TestBed.inject(UserService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+  afterEach(() => { 
+    httpMock.verify(); 
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+  it('should fetch user by ID', () => {
+    const dummyUser: User = { id: 123, pseudo: 'John Doe', email: 'johndoe@mail.com', firstname: 'John', lastname: 'Doe', dateOfBirth: '01/02/2000',roles: ['USER'] };
+
+    service.getUserById('123').subscribe(user => {
+      expect(user).toEqual(dummyUser);
+    });
+
+    const req = httpMock.expectOne(`${service['pathService']}/123`);
+    expect(req.request.method).toBe('GET');
+    req.flush(dummyUser);
+  });
+  it('should update user profile', () => {
+    const formData: FormData = new FormData();
+    const response: MessageResponse = { message: 'Profile updated successfully' };
+
+    service.updateProfile(formData, '123').subscribe(res => {
+      expect(res.message).toBe('Profile updated successfully');
+    });
+
+    const req = httpMock.expectOne(`${service['pathService']}/123`);
+    expect(req.request.method).toBe('PUT');
+    req.flush(response);
+  });
+  it('should delete user account', () => {
+    service.deleteAccount('123').subscribe(response => {
+      expect(response).toBeNull();
+    });
+
+    const req = httpMock.expectOne(`${service['pathService']}/123`);
+    expect(req.request.method).toBe('DELETE');
+    req.flush(null);
+  });
 });
+
